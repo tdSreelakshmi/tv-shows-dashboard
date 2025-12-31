9728a578175fd6cf35d5034b4356be439e755e65
tdSreelakshmi <tdsreelakshmi01@gmail.com>
2026-01-01 02:16:43 +0530

    eslint error fix

--- src/indexeddb.js
+++ src/indexeddb.js
@@ -1,84 +1,84 @@
-let db;
+let db
 
 export function openDB() {
   return new Promise((resolve, reject) => {
     if (!window.indexedDB) {
-      return reject("IndexedDB is not supported in this browser.");
+      return reject(new Error('IndexedDB is not supported in this browser.'))
     }
-    if (db) return resolve(db);
-    const request = indexedDB.open("ShowsDatabase", 1);
+    if (db) return resolve(db)
+    const request = indexedDB.open('ShowsDatabase', 1)
 
-    request.onerror = () => reject("Failed to open IndexedDB");
+    request.onerror = () => reject(new Error('Failed to open IndexedDB'))
 
     request.onupgradeneeded = (event) => {
-      const database = event.target.result;
-      if (!database.objectStoreNames.contains("shows")) {
-        database.createObjectStore("shows", { keyPath: "id" });
+      const database = event.target.result
+      if (!database.objectStoreNames.contains('shows')) {
+        database.createObjectStore('shows', { keyPath: 'id' })
       }
 
-      if (!database.objectStoreNames.contains("genres")) {
-        database.createObjectStore("genres", { keyPath: "name" });
+      if (!database.objectStoreNames.contains('genres')) {
+        database.createObjectStore('genres', { keyPath: 'name' })
       }
-    };
+    }
 
     request.onsuccess = (event) => {
-      db = event.target.result;
-      resolve(db);
-    };
-  });
+      db = event.target.result
+      resolve(db)
+    }
+  })
 }
 
 export async function addShows(shows) {
-  const database = await openDB();
+  const database = await openDB()
   return new Promise((resolve, reject) => {
-    const transaction = database.transaction("shows", "readwrite");
-    const store = transaction.objectStore("shows");
+    const transaction = database.transaction('shows', 'readwrite')
+    const store = transaction.objectStore('shows')
     for (const [id, show] of Object.entries(shows)) {
-      store.put({ id, shows: JSON.parse(JSON.stringify(show)) });
+      store.put({ id, shows: JSON.parse(JSON.stringify(show)) })
     }
 
-    transaction.oncomplete = () => resolve();
-    transaction.onerror = () => reject("Error adding shows");
-  });
+    transaction.oncomplete = () => resolve()
+    transaction.onerror = () => reject(new Error('Error adding shows'))
+  })
 }
 export async function addGenres(genres) {
-  const database = await openDB();
+  const database = await openDB()
 
   return new Promise((resolve, reject) => {
-    const transaction = database.transaction("genres", "readwrite");
-    const store = transaction.objectStore("genres");
+    const transaction = database.transaction('genres', 'readwrite')
+    const store = transaction.objectStore('genres')
 
     for (const [name, showIds] of Object.entries(genres)) {
       store.put({
         name,
-        ids: JSON.parse(JSON.stringify(showIds)),
-      });
+        ids: JSON.parse(JSON.stringify(showIds))
+      })
     }
 
-    transaction.oncomplete = () => resolve();
-    transaction.onerror = () => reject("Error adding genres");
-  });
+    transaction.oncomplete = () => resolve()
+    transaction.onerror = () => reject(new Error('Error adding genres'))
+  })
 }
 
 export async function getAllShows() {
-  const database = await openDB();
+  const database = await openDB()
   return new Promise((resolve, reject) => {
-    const transaction = database.transaction("shows", "readonly");
-    const store = transaction.objectStore("shows");
-    const request = store.getAll();
+    const transaction = database.transaction('shows', 'readonly')
+    const store = transaction.objectStore('shows')
+    const request = store.getAll()
 
-    request.onsuccess = () => resolve(request.result);
-    request.onerror = () => reject("Error fetching shows");
-  });
+    request.onsuccess = () => resolve(request.result)
+    request.onerror = () => reject(new Error('Error fetching shows'))
+  })
 }
 export async function getAllGenres() {
-  const database = await openDB();
+  const database = await openDB()
   return new Promise((resolve, reject) => {
-    const transaction = database.transaction("genres", "readonly");
-    const store = transaction.objectStore("genres");
-    const request = store.getAll();
+    const transaction = database.transaction('genres', 'readonly')
+    const store = transaction.objectStore('genres')
+    const request = store.getAll()
 
-    request.onsuccess = () => resolve(request.result);
-    request.onerror = () => reject("Error fetching shows");
-  });
+    request.onsuccess = () => resolve(request.result)
+    request.onerror = () => reject(new Error('Error fetching shows'))
+  })
 }
